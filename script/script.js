c87925aef5d0c78be7fc3875649bd99bbd5c8c25
Neroslike <obduvi22@hotmail.com>
2022-02-08 16:05:34 -0400

    Add styles and buttons Add new logic for showing/hiding the grid

--- script/script.js
+++ script/script.js
@@ -8,13 +8,17 @@ let slider = document.querySelector('#slider')
 let sizing = document.querySelector('#sizing')
 let eraserBtn = document.querySelector('#eraser')
 let colorBtn = document.querySelector('#color')
+colorBtn.classList.toggle('active')
 let rainbow = document.querySelector('#rainbow')
 let colorPicker = document.querySelector('#picker')
+let showGrid = document.querySelector('#showGrid')
 let currentColor = DEFAULT_COLOR
 let mouseDown = false
 let rain = false
+let btns = document.querySelectorAll('.mode')
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
+let gridElement = document.querySelectorAll('.grid-element')
 createGrid()
 
 function clearGrid(){
@@ -34,6 +38,7 @@ function createGrid(size = DEFAULT_SIZE) {
         div.addEventListener('mousedown', paint)
         container.appendChild(div)
     }
+    gridElement = document.querySelectorAll('.grid-element')
 }
 
 function setColor(setCol = DEFAULT_COLOR){
@@ -87,17 +92,45 @@ function clear(){
 
 btn.addEventListener('click', clear)
 
-sizing.textContent = `Value: ${slider.value}x${slider.value}`
+sizing.textContent = `Size: ${slider.value}x${slider.value}`
 slider.addEventListener('input', () => {
-    sizing.textContent = `Value: ${slider.value}x${slider.value}`
+    sizing.textContent = `Size: ${slider.value}x${slider.value}`
 })
 
 slider.addEventListener('change', () => {
     size = slider.value
     size = size * size
     renderGrid(size)
+    gridShow('remove')
+    showGrid.classList.remove('active')
 })
 
 colorPicker.addEventListener('change', (e) =>{
     setColor(e.target.value)
+})
+
+btns.forEach(element => {
+    element.addEventListener('click', (e) =>{
+        btns.forEach(butt => {
+            butt.classList.remove('active')
+        })
+        e.target.classList.toggle('active')
+    })
+});
+
+function gridShow(action){
+    if(action === 'toggle'){
+        gridElement.forEach(element => {
+            element.classList.toggle('showGrid')
+        });
+    } else if(action === 'remove'){
+        gridElement.forEach(element => {
+        element.classList.remove('showGrid')
+        });
+    }
+}
+
+showGrid.addEventListener('click', (e) =>{
+    e.target.classList.toggle('active')
+    gridShow('toggle')
 })
