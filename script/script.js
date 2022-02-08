a1d59db21c7265037a306dcec9a3e7e1f6045cbf
Neroslike <obduvi22@hotmail.com>
2022-02-08 13:11:43 -0400

    Add rainbow button Add eraser button Add color picker

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
+const DEFAULT_COLOR = 'black'
+const DEFAULT_SIZE = 16*16
+let color = DEFAULT_COLOR
 let container = document.querySelector('.container')
 let contWidth = container.clientWidth
 let btn = document.querySelector('#clear')
 let slider = document.querySelector('#slider')
 let sizing = document.querySelector('#sizing')
-const DEFAULT_SIZE = 16*16
+let eraserBtn = document.querySelector('#eraser')
+let colorBtn = document.querySelector('#color')
+let rainbow = document.querySelector('#rainbow')
+let colorPicker = document.querySelector('#picker')
+let currentColor = DEFAULT_COLOR
 let mouseDown = false
+let rain = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 createGrid()
@@ -24,10 +32,34 @@ function createGrid(size = DEFAULT_SIZE) {
         div.style.height = `${contWidth / Math.sqrt(size)}px`
         div.addEventListener('mouseover', paint)
         div.addEventListener('mousedown', paint)
-        div.addEventListener('click', paint)
         container.appendChild(div)
     }
 }
+
+function setColor(setCol = DEFAULT_COLOR){
+    color = setCol
+}
+
+eraserBtn.addEventListener('click', () => {
+    rain = false
+    setColor('white')
+})
+
+colorBtn.addEventListener('click', () => {
+    rain = false
+    setColor(colorPicker.value)
+})
+
+function rainbowMode(){
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    setColor(`rgb(${r}, ${g}, ${b})`)
+}
+
+rainbow.addEventListener('click', () => {
+    rain = true
+})
 
 function renderGrid(size){
     clearGrid()
@@ -37,15 +69,19 @@ function renderGrid(size){
 function paint(e){
     if(e.type === 'mouseover' && !mouseDown){
         return
-    } else if(mouseDown || e.type === 'click'){
-        e.target.style.background  = 'black'
+    } else if(mouseDown || e.type === 'mousedown'){
+        if(rain){
+            rainbowMode()
+            e.target.style.background = color
+        }
+        e.target.style.background = color
     }
 }
 
 function clear(){
     let divGrid = document.querySelectorAll('.grid-element')
     divGrid.forEach(div => {
-        div.style.background = ('white')
+        div.style.background = 'white'
     });
 }
 
@@ -60,4 +96,8 @@ slider.addEventListener('change', () => {
     size = slider.value
     size = size * size
     renderGrid(size)
+})
+
+colorPicker.addEventListener('change', (e) =>{
+    setColor(e.target.value)
 })
