1ffcce16dfcbc6c8c0d34acb74d2b175a65c9a0c
Neroslike <obduvi22@hotmail.com>
2022-02-08 11:26:49 -0400

    Refactor of the entire code

--- script/script.js
+++ script/script.js
@@ -1,38 +1,57 @@
 let container = document.querySelector('.container')
 let contWidth = container.clientWidth
 let btn = document.querySelector('#clear')
+let slider = document.querySelector('#slider')
+let sizing = document.querySelector('#sizing')
+const DEFAULT_SIZE = 16*16
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 createGrid()
 
-function createGrid(size = 16*16){
+function clearGrid(){
     let grid = document.querySelectorAll('.grid-element')
     grid.forEach(element => {
         element.remove()
     });
+}
+
+function createGrid(size = DEFAULT_SIZE) {
     for(let i = 1;i < size+1; i++){
         let div = document.createElement('div')
         div.classList.add('grid-element')
         div.style.width = `${contWidth / Math.sqrt(size)}px`
         div.style.height = `${contWidth / Math.sqrt(size)}px`
-        div.addEventListener('mouseover', () => {
-            div.style.background = 'black'
-        })
+        div.addEventListener('mouseover', paint)
+        div.addEventListener('mousedown', paint)
+        div.addEventListener('click', paint)
         container.appendChild(div)
     }
 }
 
-btn.addEventListener('click', () => {
+function renderGrid(size){
+    clearGrid()
+    createGrid(size)
+}
+
+function paint(e){
+    if(e.type === 'mouseover' && !mouseDown){
+        return
+    } else if(mouseDown || e.type === 'click'){
+        e.target.style.background  = 'black'
+    }
+}
+
+function clear(){
     let divGrid = document.querySelectorAll('.grid-element')
     divGrid.forEach(div => {
         div.style.background = ('white')
     });
-})
-let slider = document.querySelector('#slider')
-function showSliderVal(){
 }
 
-let sizing = document.querySelector('#sizing')
-sizing.textContent = `Value: ${slider.value}x${slider.value}`
+btn.addEventListener('click', clear)
 
+sizing.textContent = `Value: ${slider.value}x${slider.value}`
 slider.addEventListener('input', () => {
     sizing.textContent = `Value: ${slider.value}x${slider.value}`
 })
@@ -40,5 +59,5 @@ slider.addEventListener('input', () => {
 slider.addEventListener('change', () => {
     size = slider.value
     size = size * size
-    createGrid(size)
+    renderGrid(size)
 })
